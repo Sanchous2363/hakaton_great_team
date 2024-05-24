6f87e75d926f2d8e0872572787d275e708f1e9e6
Sanchous2363 <151240556+Sanchous2363@users.noreply.github.com>
2024-05-24 17:53:34 +0300

    Update config.py

--- СБОРКА!!!/config.py
+++ СБОРКА!!!/config.py
@@ -1,9 +1,9 @@
 LOGS = 'logs.txt'  # файл для логов
 
-IAM_TOKEN = "t1.9euelZqYm5zNnJCTlZGWipmaipidl-3rnpWanJSKkMjHis3Ijc-ZiY7LjJ7l9PdpZj5N-e89Cy233fT3KRU8TfnvPQstt83n9euelZqblc3Iz5fJz5uezIudk56Nk-_8xeuelZqblc3Iz5fJz5uezIudk56Nk73rnpWaj52Pj8vNkcecypmZkMibkZ213oac0ZyQko-Ki5rRi5nSnJCSj4qLmtKSmouem56LntKMng.WODTWjt32qsSyAxEehzcV6wFafSlcauf7rveicLani6Q9gXJ1XIkhgQdoPLSvmcczOw3jb3GWvc2JyBESMQoBg"
-FOLDER_ID = "b1g7scu9jilatp911rei"
-API_KEY = "d1ef3b03ce19cb4a66cc5b125faa5ecb"
-BOT_TOKEN = "7163123182:AAGnClmzVDMngdRB-T4ta4uCkoLceDx5WPQ"
+IAM_TOKEN = ""
+FOLDER_ID = ""
+API_KEY = ""
+BOT_TOKEN = ""
 MAX_GPT_TOKENS = 100
 DESCRIPTION_CITY = [{'role': 'system', 'text':  "Ты - бот, который знает все о городах. Не пиши поясняющего текста! Расскажи кратко о городе"}]
 IF_WEATHER_CLOTHES = [{'role': 'system', 'text':  " Не пиши поясняющего текста! Скажи кратко, что одеть при температуре"}]
